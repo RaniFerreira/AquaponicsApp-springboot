825101df7895cba398562799e3cd69d5b7a9a5ec
RaniFerreira <franielly535@gmail.com>
2026-03-31 17:13:55 -0300

    adicao do metodo de listar no controller e ajuste  para listar  na interface

--- src/main/java/br/edu/iftm/aquaponicsapp/controller/TanqueController.java
+++ src/main/java/br/edu/iftm/aquaponicsapp/controller/TanqueController.java
@@ -1,14 +1,22 @@
 package br.edu.iftm.aquaponicsapp.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.edu.iftm.aquaponicsapp.service.TanqueService;
 
 @Controller
 public class TanqueController {
+
+    @Autowired
+    private TanqueService TanqueService;
    
      @GetMapping("/tanque")
-    public String getMethodName() {
+     public String index(Model model) {
+        model.addAttribute("tanque", TanqueService.getAllTanque());
         return "/tanque/index";
     }
 }
