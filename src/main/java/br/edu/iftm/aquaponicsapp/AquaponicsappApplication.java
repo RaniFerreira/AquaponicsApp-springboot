4811540300d9dce1820f01040e709478ec764a02
RaniFerreira <franielly535@gmail.com>
2026-03-31 15:09:58 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/aquaponicsapp/AquaponicsappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.aquaponicsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaponicsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaponicsappApplication.class, args);
+	}
+
+}
